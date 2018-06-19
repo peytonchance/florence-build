66c1bd1eaaf91afaa896f17a9002f070db251cc7
peytonchance <peytonchance@gmail.com>
2018-06-19 11:59:19 -0400

    First take on on-load pill animations. Some minor JS refactoring.

--- src/javascripts/modules/pills.js
+++ src/javascripts/modules/pills.js
@@ -1,28 +1,39 @@
 export default class Pills {
   constructor(el) {
     this.el = el
-    this.setVars()
     this.setupListeners()
   }
 
-  setVars() {
-    this.pillVariants = ['yellow-', '', 'more-', 'purple-']
-  }
-
   setupListeners() {
-    window.addEventListener('load', () => this.setupTimeout())
+    window.addEventListener('load', () => this.handleLoad())
   }
 
-  setupTimeout() {
-    this.timeout = (this.animatePills(), 200)
+  handleLoad() {
+    clearTimeout(this.timeout)
+    this.timeout = setTimeout(() => this.animatePills(), 500)
+    this.releaseSqueezeTimeout = setTimeout(() => this.releasePillSqueeze(), 4500)
   }
 
   animatePills() {
-    console.log(this.pillVariants.length);
-    var i;
-    for (i=0; i < this.pillVariants.length; i++) {
-      this.rollClass = this.pillVariants[i] + 'pills-roll'
-      this.el.classList.add(this.rollClass)
+    if (this.el.classList.contains('yellow-pill')) {
+      this.el.classList.add('yellow-pill-roll')
+    } else if (this.el.classList.contains('more-pills')) {
+      this.el.classList.add('more-pills-roll')
+    } else if (this.el.classList.contains('purple-pills')) {
+      this.el.classList.add('purple-pills-roll')
+    } else if(this.el.classList.contains('title-tablet')) {
+      this.el.classList.add('title-tablet-roll')
+    } else if(this.el.classList.contains('title-pill-red')) {
+      this.el.classList.add('title-pill-red-bounce')
+    } else {
+      this.el.classList.add('pills-roll')
+    }
+  }
+
+  releasePillSqueeze() {
+    if (this.el.classList.contains('title-pill-red-bounce')) {
+      console.log('did i wait?')
+      this.el.classList.remove('title-pill-red-bounce')
     }
   }
 }
