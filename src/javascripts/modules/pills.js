7a572c69150c7249f83db731de4ef1e6ec39abc7
peytonchance <peytonchance@gmail.com>
2018-06-19 17:23:37 -0400

    First half of PR16 edits.

--- src/javascripts/modules/pills.js
+++ src/javascripts/modules/pills.js
@@ -9,31 +9,18 @@ export default class Pills {
   }
 
   handleLoad() {
-    clearTimeout(this.timeout)
     this.timeout = setTimeout(() => this.animatePills(), 500)
     this.releaseSqueezeTimeout = setTimeout(() => this.releasePillSqueeze(), 4500)
   }
 
   animatePills() {
-    if (this.el.classList.contains('yellow-pill')) {
-      this.el.classList.add('yellow-pill-roll')
-    } else if (this.el.classList.contains('more-pills')) {
-      this.el.classList.add('more-pills-roll')
-    } else if (this.el.classList.contains('purple-pills')) {
-      this.el.classList.add('purple-pills-roll')
-    } else if(this.el.classList.contains('title-tablet')) {
-      this.el.classList.add('title-tablet-roll')
-    } else if(this.el.classList.contains('title-pill-red')) {
-      this.el.classList.add('title-pill-red-bounce')
-    } else {
-      this.el.classList.add('pills-roll')
-    }
+    this.el.classList.add('pills-animating')
   }
 
   releasePillSqueeze() {
-    if (this.el.classList.contains('title-pill-red-bounce')) {
-      console.log('did i wait?')
-      this.el.classList.remove('title-pill-red-bounce')
+    var tempPill = this.el.children[1].children[0].children[0].children[0]
+    if (tempPill.classList.contains('title-pill-red-bounce')) {
+      tempPill.classList.remove('title-pill-red-bounce')
     }
   }
 }
