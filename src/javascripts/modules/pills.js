93ebbdb9a11087bcecce131fc9f71b2226551670
peytonchance <peytonchance@gmail.com>
2018-06-20 12:58:17 -0400

    Pill shake animation at top of page.

--- src/javascripts/modules/pills.js
+++ src/javascripts/modules/pills.js
@@ -1,16 +1,33 @@
 export default class Pills {
   constructor(el) {
     this.el = el
+    this.setVars()
     this.setupListeners()
+  }
+
+  setVars() {
+    this.ticking = false
+    this.lastScrollY = 0
   }
 
   setupListeners() {
     window.addEventListener('load', () => this.handleLoad())
+    window.addEventListener('scroll', () => this.handleScroll())
   }
 
   handleLoad() {
     this.timeout = setTimeout(() => this.animatePills(), 500)
     this.releaseSqueezeTimeout = setTimeout(() => this.releasePillSqueeze(), 3500)
+  }
+
+  handleScroll() {
+    this.lastScrollY = window.scrollY
+    if (!this.ticking) {
+      window.requestAnimationFrame(() => {
+        this.shakePills()
+      })
+    }
+    this.ticking = true
   }
 
   animatePills() {
@@ -21,6 +38,18 @@ export default class Pills {
     var titlePill = this.el.children[1].children[0].children[0].children[0]
     if (titlePill.classList.contains('title-pill-red-bounce')) {
       titlePill.classList.remove('title-pill-red-bounce')
+    }
+  }
+
+  shakePills() {
+    this.ticking = false
+
+    if (this.lastScrollY === 0) {
+      this.el.classList.add('pills-shaking')
+      this.el.parentNode.classList.add('overflow-visible')
+    } else {
+      this.el.classList.remove('pills-shaking')
+      this.el.parentNode.classList.remove('overflow-visible')
     }
   }
 }
